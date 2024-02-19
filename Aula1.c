caf293c8ff248fc11fc16e2da2989bd7116175c8
Pedro <pedrodecarlisilva@gmail.com>
2024-02-19 08:43:25 -0300

    Término Aula 1

---
+++ Aula1.c
@@ -0,0 +1,19 @@
+/* Aula 1  19FEV2024    
+Asp 1021 Pedro DE CARLI Silva*/
+
+#include<stdio.h>
+
+int main()          //função principal
+{
+    int idade = 0; //declara a variável inteira idade
+    int altura = 0;
+
+    printf("\n\n\nIdade: ");
+    scanf("%d",&idade);
+    printf("\nAltura: ");
+    scanf("%d",&altura);
+
+    printf("\n\nIdade digitada: %d",idade);
+    printf("\nAltura: %d",altura);
+    return 0;
+}
