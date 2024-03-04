ec39a65c9a403f883ed837efd2ad148fca9d464a
Pedro <pedrodecarlisilva@gmail.com>
2024-03-04 07:28:51 -0300

    Correção

--- Exercícios/Exec 03.c
+++ Exercícios/Exec 03.c
@@ -8,11 +8,13 @@ int main() // é a função principal
     printf("Insira um número para ser somado (-1 termina): "); // Escreve isso na tela
     int soma = 0;                                              // declara a variável soma com valor 0
     int i;                                                     // declara a variável i
+    scanf("%d", &i);                                           // lê i
 
     while (i != -1) // enquanto i for diferente de -1
     {
-        scanf("%d", &i); // lê i
         soma = soma + i; // adiciona i na variável soma
+        scanf("%d", &i); // lê i
+        printf("Insira um número para ser somado (-1 termina): ")
     }
     soma++;                           // aumenta soma em 1 (porque para parar precisamos digitar um -1, que acaba sendo somado)
     printf("\nA soma resultou em: "); // escreve o valor da soma no final
