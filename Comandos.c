e4002d1d12023f888deef1659142e8b1477f1103
Pedro <pedrodecarlisilva@gmail.com>
2024-03-24 19:26:09 -0300

    Formatei

--- Comandos.c
+++ Comandos.c
@@ -5,18 +5,18 @@ main(){
 
     return 0;
 }
-
+--------------------------------------------------------------------------------
 Ler dados -> scanf("formatação",&variavel);
-
+--------------------------------------------------------------------------------
 Imprimir dados -> printf("TEXTO"); ou printf("formatação",variável)
-
-%f -> float -> Número com decimal
-%d -> int -> Númemro inteiro
-%c -> caractere
+--------------------------------------------------------------------------------
+%f -> float -> Número com decimal = float variavel ou float variavel=10;
+%d -> int -> Númemro inteiro = int variavel
+%c -> caractere = char
 %u -> inteiro sem sinal
 %e -> notação científica
 %s -> strings
-
+--------------------------------------------------------------------------------
 COMPARAÇÃO:
 
 ==  igual
@@ -25,13 +25,13 @@ COMPARAÇÃO:
 <   menor que
 >=  maior ou igual ou
 <=  menor ou igual ou
-
+--------------------------------------------------------------------------------
 LÓGICA:
 
 &&  e
 ||  ou
 !   não
-
+--------------------------------------------------------------------------------
 SE:
 
 if (a==3 &&b <=2)
@@ -41,7 +41,7 @@ if (a==3 &&b <=2)
 else{
     CÓDIGO SENÃO
 };
-
+--------------------------------------------------------------------------------
 PULAR LINHA NO TEXTO:
 
 \n
