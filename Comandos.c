a232ed41637da4a34da196ece6c32e4be8ce43ec
Pedro <pedrodecarlisilva@gmail.com>
2024-03-25 09:41:45 -0300

    Printf corrigido

--- Comandos.c
+++ Comandos.c
@@ -8,7 +8,7 @@ main(){
 --------------------------------------------------------------------------------
 Ler dados -> scanf("formatação",&variavel);
 --------------------------------------------------------------------------------
-Imprimir dados -> printf("TEXTO E FORMATAÇÃO",var1,var2,var3[...]);
+Imprimir dados -> printf("TEXTO E FORMATAÇÃO",var1,var2,var3[...    ]);
 --------------------------------------------------------------------------------
 %f -> float -> Número com decimal = float variavel ou float variavel=10;
 %d -> int -> Númemro inteiro = int variavel
